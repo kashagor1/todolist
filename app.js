67c99dc304c2849b0f16d186147bbd9694a30f8c
unknown <ka.shagor1@gmail.com>
2023-07-19 08:17:31 +0600

    changed some input

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.get("/about",(req,res)=>{
 });
 
 
-let port = 3000;
+let port = process.env.port||3000;
 app.listen(port,()=>{
-	console.log("Server Started on Port");
+	console.log("Server Started on Port"+port);
 });
